338a386dee591cf465b08365131516a5f969dabd
李阳 <1586557814@qq.com>
2016-07-26 17:03:37 +0800

    Initial Commit

---
+++ NSTreadAndNSOperationtext/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NSTreadAndNSOperationtext
+//
+//  Created by liyangly on 16/7/26.
+//  Copyright © 2016年 liyangly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ NSTreadAndNSOperationtext/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  NSTreadAndNSOperationtext
+//
+//  Created by liyangly on 16/7/26.
+//  Copyright © 2016年 liyangly. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
